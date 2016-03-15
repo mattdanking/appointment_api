ca1993e2275692aa288dca341c6b7d64a1c26d41
Matt King <mattdanking@gmail.com>
2016-03-15 18:38:26 -0400

    added crud methods

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,51 @@
 class AppointmentsController < ApplicationController
+  before_action :set_appointment, only: [:update, :destroy]
+
+  def index
+    @appointments = Appointment.all
+  end
+
+  def new
+    @appointment = Appointment.new
+  end
+
+  def create
+    @appointment = Appointment.new(appointment_params)
+
+    respond_to do |format|
+      if @appointment.save
+        format.json { render :show, status: 200, location: @appointment }
+      else
+        format.json { render json: @appointment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @appointment.update(appointment_params)
+        format.json { render :show, status: :ok, location: @appointment }
+      else
+        format.json { render json: @appointment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @appointment.destroy
+    respond_to do |format|
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_appointment
+      @appointment = Appointment.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def appointment_params
+      params.require(:appointment).permit(:start_time, :end_time, :first_name, :last_name, :comments)
+    end
 end
